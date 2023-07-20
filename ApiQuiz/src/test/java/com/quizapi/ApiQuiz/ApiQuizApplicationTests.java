23e16e34634eae320c526db87d5c6897cc5cbf1f
Mamadou <mohamedfdg72@gmail.com>
2023-07-20 15:23:47 +0000

    premier commit

---
+++ ApiQuiz/src/test/java/com/quizapi/ApiQuiz/ApiQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quizapi.ApiQuiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiQuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
